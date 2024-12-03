dcc66788f55891391f121e4372cc976eaff01fe1
Travis <github@travisfriesen.ca>
2024-12-03 09:37:40 -0600

    Bring prod copy to latest (#5)
    * Updated logo and new execs (#2)
    
    * add cssa logo as png
    
    * updated logo and added new team members
    
    * Catching up to sept 26, 2024 (#3)
    
    * added blank image for team page
    
    * Updated team names. Awaiting Links and Photos
    
    * added new team members, and updated information
    
    * New execs + CSSA Resources (#2)
    
    * added blank image for team page
    
    * Updated team names. Awaiting Links and Photos
    
    * added new team members, and updated information
    
    * Removed link for team member
    
    * Internal Design Document and CSSA Resources
    
    * fixed link
    
    * add cssa logo as png
    
    * updated logo and added new team members
    
    * first year guide (thx peter)
    
    * Rename firstYearGuideV1.1.pdf to firstYearGuideV1-0.pdf
    
    change name
    
    * added first year guide to website
    
    * modified resource page layout, and first year guide
    
    * tou and handbook copys
    
    * updated discord tou and mod handbook on resources page
    
    * updated document
    
    * Added dockerfile
    
    * updated 3 exec photos (#5)
    
    okiki
    peter
    tofunmi
    
    * remove image for merge
    
    ---------
    
    Co-authored-by: Aryan0826 <165449842+Aryan0826@users.noreply.github.com>

--- src/data/team.ts
+++ src/data/team.ts
@@ -1,75 +1,73 @@
 interface IProfile {
-    name: string
-    position: string
-    image: string
-    linkedin?: string
-    instagram?: string
-    github?: string
-    website?: string
+  name: string;
+  position: string;
+  image: string;
+  linkedin?: string;
+  instagram?: string;
+  github?: string;
+  website?: string;
 }
 
 export const ExecProfiles = [
-    {
-        name: "Lyle Arcinas",
-        position: "President",
-        image: "/img/team/lyle.png",
-        linkedin: "https://www.linkedin.com/in/lyle-arcinas/",
-        github: "https://github.com/lylearcinas",
-        website: "https://arcinas.info/"
-    },
-    {
-        name: "Okiki Fasanmi",
-        position: "Vice President",
-        image: "/img/team/okiki.png",
-        linkedin: "https://www.linkedin.com/in/okikiola-fasanmi-b264a6203/"
-    },
-    {
-        name: "Caway Xiong",
-        position: "Director of Lounge Affairs",
-        image: "/img/team/caway.png",
-        linkedin: "https://www.linkedin.com/in/caway-xiong-465316230/",
-        github: "https://github.com/2Lazy2BeOriginal"
-    },
-    {
-        name: "Peter Vu",
-        position: "Director of Student Affairs",
-        image: "/img/team/peter.jpg",
-        linkedin: "https://linkedin.com/in/petervu298/",
-        github: "https://github.com/pieberrykinnie",
-        discord: "https://discord.com/users/363279137768734720"
-    },
-    {
-        name: "Tofunmi Layi-Babatunde",
-        position: "Director of Advocacy",
-        image: "/img/team/tofunmi.jpg",
-        linkedin: "https://www.linkedin.com/in/tofunmi-layi-babatunde-3477ab30a/"
-    },
-    {
-        name: "Edwin Duong",
-        position: "Director of Internal Affairs",
-        image: "/img/team/edwin.jpg",
-        linkedin: "https://www.linkedin.com/in/edwin-duong/"
-    },
-    {
-        name: "Travis Friesen",
-        position: "Director of Promotions",
-        image: "/img/team/travis.jpg",
-        linkedin: "https://www.linkedin.com/in/travisfriesen/",
-        github: "https://github.com/travisfriesen",
-        website: "https://travisfriesen.ca"
-    },
-    {
-        name: "Chinu Chandaria",
-        position: "Director of Events",
-        image: "/img/team/chinu.png",
-        linkedin: "https://www.linkedin.com/in/chaahna-c-5781b9263"
-
-    },
-    {
-        name: "Ginelle Temull",
-        position: "Director of Finance",
-        image: "/img/team/ginelle.png",
-        linkedin: "https://www.linkedin.com/in/ginelle-temull"
-
-    },
-]
+  {
+    name: "Lyle Arcinas",
+    position: "President",
+    image: "/img/team/lyle.png",
+    linkedin: "https://www.linkedin.com/in/lyle-arcinas/",
+    github: "https://github.com/lylearcinas",
+    website: "https://arcinas.info/",
+  },
+  {
+    name: "Okiki Fasanmi",
+    position: "Vice President",
+    image: "/img/team/okiki.png",
+    linkedin: "https://www.linkedin.com/in/okikiola-fasanmi-b264a6203/",
+  },
+  {
+    name: "Caway Xiong",
+    position: "Director of Lounge Affairs",
+    image: "/img/team/caway.png",
+    linkedin: "https://www.linkedin.com/in/caway-xiong-465316230/",
+    github: "https://github.com/2Lazy2BeOriginal",
+  },
+  {
+    name: "Peter Vu",
+    position: "Director of Student Affairs",
+    image: "/img/team/peter.png",
+    linkedin: "https://linkedin.com/in/petervu298/",
+    github: "https://github.com/pieberrykinnie",
+    discord: "https://discord.com/users/363279137768734720",
+  },
+  {
+    name: "Tofunmi Layi-Babatunde",
+    position: "Director of Advocacy",
+    image: "/img/team/tofunmi.png",
+    linkedin: "https://www.linkedin.com/in/tofunmi-layi-babatunde-3477ab30a/",
+  },
+  {
+    name: "Edwin Duong",
+    position: "Director of Internal Affairs",
+    image: "/img/team/edwin.jpg",
+    linkedin: "https://www.linkedin.com/in/edwin-duong/",
+  },
+  {
+    name: "Travis Friesen",
+    position: "Director of Promotions",
+    image: "/img/team/travis.jpg",
+    linkedin: "https://www.linkedin.com/in/travisfriesen/",
+    github: "https://github.com/travisfriesen",
+    website: "https://travisfriesen.ca",
+  },
+  {
+    name: "Chinu Chandaria",
+    position: "Director of Events",
+    image: "/img/team/chinu.png",
+    linkedin: "https://www.linkedin.com/in/chaahna-c-5781b9263",
+  },
+  {
+    name: "Ginelle Temull",
+    position: "Director of Finance",
+    image: "/img/team/ginelle.png",
+    linkedin: "https://www.linkedin.com/in/ginelle-temull",
+  },
+];
