2ad74af83396ad829ee1845012106909ed883ec2
Aryan0826 <165449842+Aryan0826@users.noreply.github.com>
2025-02-15 16:43:15 -0600

    updated contact form
    react-form-hook, create send-email.tx util

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,23 +1,39 @@
+"use client";
+
 import BlockHeader from "@/components/block-header";
 import PageHeader from "@/components/page-header";
 import ProfileCard from "@/components/profile-card";
 import { ExecProfiles } from "@/data/team";
+import { FC } from "react";
+import { useForm } from "react-hook-form";
+import sendEmail from "@/utils/send-email";
 
 export default function Sponsor() {
-
-    return (
-        <main className="flex flex-col">
-            <PageHeader title="Contact Us" image="/img/backgrounds/contact.jpg" />
-            <div className="container py-12 flex flex-col gap-12">
-                <div className="flex flex-col gap-8">
-                    <BlockHeader title="Contact Email" />
-                    <p>If you'd like to get in touch by email, please contact us at <a className="text-cssa-gold hover:underline" href="mailto:cssa@umanitoba.ca">cssa@umanitoba.ca</a></p>
-                </div>
-                <div className="flex flex-col gap-8">
-                    <BlockHeader title="Contact Form" />
-                    <iframe title='Contact us form' className="border border-solid border-gray-400 w-full min-h-[1500px]" src="https://forms.office.com/Pages/ResponsePage.aspx?id=C92AT4wzTE6KFJBEaWL3uMetWVcCpo5KtSmErSAZ7GlUM1JYMFNPNURWWENLSzFONE5ZWUlURzNZOC4u&embed=true" />
-                </div>
-            </div>
-        </main>
-    );
+  return (
+    <main className="flex flex-col">
+      <PageHeader title="Contact Us" image="/img/backgrounds/contact.jpg" />
+      <div className="container py-12 flex flex-col gap-12">
+        <div className="flex flex-col gap-8">
+          <BlockHeader title="Contact Email" />
+          <p>
+            If you'd like to get in touch by email, please contact us at{" "}
+            <a
+              className="text-cssa-gold hover:underline"
+              href="mailto:cssa@umanitoba.ca"
+            >
+              cssa@umanitoba.ca
+            </a>
+          </p>
+        </div>
+        <div className="flex flex-col gap-8">
+          <BlockHeader title="Contact Form" />
+          <iframe
+            title="Contact us form"
+            className="border border-solid border-gray-400 w-full min-h-[1500px]"
+            src="https://forms.office.com/Pages/ResponsePage.aspx?id=C92AT4wzTE6KFJBEaWL3uMetWVcCpo5KtSmErSAZ7GlUM1JYMFNPNURWWENLSzFONE5ZWUlURzNZOC4u&embed=true"
+          />
+        </div>
+      </div>
+    </main>
+  );
 }
---
+++ src/utils/send-email.ts
@@ -0,0 +1,3 @@
+export default function sendEmail() {
+  //api call for sending email
+}
