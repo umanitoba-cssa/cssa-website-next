ed3d8f2bbd49d32e1b646234c8181a34e93c7974
Aryan0826 <165449842+Aryan0826@users.noreply.github.com>
2025-02-16 02:24:49 -0600

    complete the send email functionality
    contact form now works

--- src/utils/send-email.ts
+++ src/utils/send-email.ts
@@ -1,6 +1,17 @@
 import { FormData } from "@/app/contact/page";
 
 export default function sendEmail(data: FormData) {
-  //api call for sending email
-  console.log(data);
+  const apiEndpoint = "/api/email";
+
+  fetch(apiEndpoint, {
+    method: "POST",
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((response) => {
+      alert(response.message);
+    })
+    .catch((err) => {
+      alert(err);
+    });
 }
