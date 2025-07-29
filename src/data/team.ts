f83b39e6b75310de5918ae0db34151d0b95567c7
Aryan0826 <165449842+Aryan0826@users.noreply.github.com>
2025-07-28 23:33:58 -0500

    Update Execs Profile page
    No new photos/links yet, update only names and positions

--- src/data/team.ts
+++ src/data/team.ts
@@ -1,79 +1,66 @@
 interface IProfile {
-  name: string;
-  position: string;
-  image: string;
-  linkedin?: string;
-  instagram?: string;
-  github?: string;
-  website?: string;
+    name: string;
+    position: string;
+    image: string;
+    linkedin?: string;
+    instagram?: string;
+    github?: string;
+    website?: string;
 }
 
 export const ExecProfiles = [
-  {
-    name: "Lyle Arcinas",
-    position: "President",
-    image: "/img/team/lyle.png",
-    linkedin: "https://www.linkedin.com/in/lyle-arcinas/",
-    github: "https://github.com/lylearcinas",
-    website: "https://arcinas.info/",
-  },
-  {
-    name: "Okiki Fasanmi",
-    position: "Vice President",
-    image: "/img/team/okiki.png",
-    linkedin: "https://www.linkedin.com/in/okikiola-fasanmi-b264a6203/",
-  },
-  {
-    name: "Caway Xiong",
-    position: "Director of Lounge Affairs",
-    image: "/img/team/caway.png",
-    linkedin: "https://www.linkedin.com/in/caway-xiong-465316230/",
-    github: "https://github.com/2Lazy2BeOriginal",
-  },
-  {
-    name: "Peter Vu",
-    position: "Director of Student Affairs",
-    image: "/img/team/peter.png",
-    linkedin: "https://linkedin.com/in/petervu298/",
-    github: "https://github.com/pieberrykinnie",
-    discord: "https://discord.com/users/363279137768734720",
-  },
-  {
-    name: "Tofunmi Layi-Babatunde",
-    position: "Director of Advocacy",
-    image: "/img/team/tofunmi.png",
-    linkedin: "https://www.linkedin.com/in/tofunmi-layi-babatunde-3477ab30a/",
-  },
-  {
-    name: "Edwin Duong",
-    position: "Director of Internal Affairs",
-    image: "/img/team/edwin.png",
-    linkedin: "https://www.linkedin.com/in/edwin-duong/",
-  },
-  {
-    name: "Travis Friesen",
-    position: "Director of Promotions",
-    image: "/img/team/travis.png",
-    linkedin: "https://www.linkedin.com/in/travisfriesen/",
-    github: "https://github.com/travisfriesen",
-    website: "https://travisfriesen.ca",
-  },
-  {
-    name: "Chinu Chandaria",
-    position: "Director of Events",
-    image: "/img/team/chinu.png",
-    linkedin: "https://www.linkedin.com/in/chaahna-c-5781b9263",
-  },
-  {
-    name: "Ginelle Temull",
-    position: "Director of Finance",
-    image: "/img/team/ginelle.png",
-    linkedin: "https://www.linkedin.com/in/ginelle-temull",
-  },
-  {
-    name: "KC del Moro",
-    position: "Assistant Director of Promotions",
-    image: "/img/team/kc.png",
-    linkedin: "https://www.linkedin.com/in/kriza-del-moro-950378301/",
-  },
+    {
+        name: "Travis Friesen",
+        position: "President",
+        image: "/img/team/vacant.png",
+        linkedin: "https://www.linkedin.com/in/travisfriesen/",
+        github: "https://github.com/travisfriesen",
+        website: "https://travisfriesen.ca",
+    },
+    {
+        name: "Okiki Fasanmi",
+        position: "Vice President",
+        image: "/img/team/vacant.png",
+        linkedin: "https://www.linkedin.com/in/okikiola-fasanmi-b264a6203/",
+    },
+    {
+        name: "Edith",
+        position: "Director of Lounge Affairs",
+        image: "/img/team/vacant.png",
+    },
+    {
+        name: "Sheikh Adeeb",
+        position: "Director of Student Affairs",
+        image: "/img/team/vacant.png",
+    },
+    {
+        name: "Richard Shalchi",
+        position: "Director of Advocacy",
+        image: "/img/team/vacant.png",
+    },
+    {
+        name: "Timmy",
+        position: "Director of Internal Affairs",
+        image: "/img/team/vacant.png",
+    },
+    {
+        name: "Ara Santos",
+        position: "Director of Technology",
+        image: "/img/team/vacant.png",
+    },
+    {
+        name: "Lindsay",
+        position: "Director of Events",
+        image: "/img/team/vacant.png",
+    },
+    {
+        name: "Ethan Ganas",
+        position: "Director of Finance",
+        image: "/img/team/vacant.png",
+    },
+    {
+        name: "KC del Moro",
+        position: "Assistant Director of Promotions",
+        image: "/img/team/vacant.png",
+    },
 ];
