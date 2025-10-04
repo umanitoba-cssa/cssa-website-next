cc4ca2214cc6f41e8d39ddaf2ca38d37b6e8c94b
cjlangan <connorjameslangan@gmail.com>
2025-10-04 13:21:43 -0500

    feat: added minimal code for Researchers Directory landing page

---
+++ src/app/resources/researchers-directory/page.tsx
@@ -0,0 +1,9 @@
+import PageHeader from "@/components/page-header";
+
+export default async function ResearchersDirectory() {
+    return (
+        <main className="flex flex-col">
+            <PageHeader title="Researchers Directory" image="/img/backgrounds/resources.png" />
+        </main>
+    );
+}
