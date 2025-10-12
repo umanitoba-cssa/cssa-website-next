249b3d165431f8e6b236e62818cf121100d3f1cb
Aidan McLeod <45048893+ACM02@users.noreply.github.com>
2025-10-11 21:58:42 -0500

    Improve researcher card styling
    - Images now take the entire height of the card
    - View Researcher text is bottom aligned and rest is top aligned

--- src/components/researcher-card.tsx
+++ src/components/researcher-card.tsx
@@ -19,56 +19,58 @@ export default function ResearcherCard({
             onKeyDown={(e) => {
                 if (e.key === "Enter" || e.key === " ") onClick?.();
             }}
-            className="flex w-full border-solid border border-amber-400 rounded-xl items-center p-4 gap-4 max-w-sm cursor-pointer"
+            className="flex w-full border-solid border border-amber-400 rounded-xl items-start p-4 gap-4 max-w-[27rem] cursor-pointer"
         >
-            <div className="w-1/3">
-            {/* TODO: Magic numbers for dimensions */}
-                <Image src={profile?.image} alt={profile?.fullName} width={400} height={400} className="rounded-xl" />
+            <div className="w-2/5 relative h-full">
+            <Image
+                src={profile?.image}
+                alt={profile?.fullName}
+                fill
+                className="object-cover object-center rounded-xl"
+            />
             </div>
-            <div className="w-2/3">
+            <div className="w-3/5 flex flex-col h-full">
                 <div className="text-xl font-bold">
                     {profile?.fullName}
                 </div>
+
                 <div className="flex flex-col gap-2 mt-1 w-fit">
-                {profile?.acceptingStudents && (
+                    {profile?.acceptingStudents && (
                     <span className="inline-flex items-center w-fit rounded-md bg-lime-700 px-2 py-1 text-xs font-medium text-white">
-                    Accepting Student Researcher(s)
+                        Accepting Student Researcher(s)
                     </span>
-                )}
-                {profile?.lab && (
+                    )}
+                    {profile?.lab && (
                     <span className="inline-flex items-center w-fit rounded-md bg-yellow-900 px-2 py-1 text-xs font-medium text-white">
-                    {profile?.lab?.name}
+                        {profile?.lab?.name}
                     </span>
-                )}
+                    )}
                 </div>
-                
-                <div className="font-semibold mt-2">
-                    Research Interests
-                </div>
+
+                <div className="font-semibold mt-2">Research Interests</div>
                 <ul className="space-y-1">
                     {profile?.researchInterests.map((interest, index) => (
-                        <li
+                    <li
                         key={index}
                         className="relative pl-4 text-sm line-clamp-1 before:content-['•'] before:absolute before:left-0 before:text-white"
-                        >
+                    >
                         {interest}
-                        </li>
+                    </li>
                     ))}
                 </ul>
-                <div className="font-semibold mt-2">
-                    Current Research Topic
-                </div>
-                {profile?.researchTopic &&
-                <ul className="space-y-1">
+
+                <div className="font-semibold mt-2">Current Research Topic</div>
+                {profile?.researchTopic && (
+                    <ul className="space-y-1">
                     <li className="relative pl-4 text-sm line-clamp-2 before:content-['•'] before:absolute before:left-0 before:text-white">
                         {profile?.researchTopic}
                     </li>
-                </ul>
-                }
-                <div className="mt-1 text-sm text-gray-500 font-medium text-right gap-1">
+                    </ul>
+                )}
+                <div className="mt-auto pt-2 text-sm text-gray-500 font-medium text-right gap-1 flex justify-end">
                     <span>View Researcher</span>
                     <span>→</span>
-                </div> 
+                </div>
             </div>
         </motion.div>
     );
