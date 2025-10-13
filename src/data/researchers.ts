db972a4bab1399a87c6a7dfa70a8d94f7d3d296a
Aidan McLeod <45048893+ACM02@users.noreply.github.com>
2025-10-12 19:31:45 -0500

    Revert changes to page.tsx and researchers.ts

--- src/data/researchers.ts
+++ src/data/researchers.ts
@@ -9,57 +9,8 @@ export interface IResearcher { // researcher properties
     acceptingStudents: boolean;
     minStudentRequirements: string[];
 }
-
 export interface ILab {
   name: string;
   website?: string;
   description: string;
 }
-
-export const ResearcherProfiles: IResearcher[] = [
-  {
-    fullName: "Dr. Jane Smith",
-    email: "me@you.us",
-    image: "/img/team/travis.png",
-    googleScholarLink: "https://google.ca",
-    lab: {
-      name: "HCI Lab",
-      website: "hci.cs.umanitoba.ca",
-      description: "HCI lol",
-    },
-    researchInterests: ["Human Computer Interaction","Science Science Too Long to fit all onto one line like omg that's so sad"],
-    researchTopic: "Something something super good but is just too long to fit here but I want to see the dots show up lol",
-    acceptingStudents: true,
-    minStudentRequirements: ["GPA 5+"]
-  },
-    {
-    fullName: "Dr. Jane Smith",
-    email: "me@you.us",
-    image: "/img/team/travis.png",
-    googleScholarLink: "https://google.ca",
-    lab: {
-      name: "HCI Lab",
-      website: "hci.cs.umanitoba.ca",
-      description: "HCI lol",
-    },
-    researchInterests: ["Human Computer Interaction","Science Science Too Long to fit all onto one line like omg that's so sad"],
-    researchTopic: "Something something super good but is just too long to fit here but I want to see the dots show up lol",
-    acceptingStudents: true,
-    minStudentRequirements: ["GPA 5+"]
-  },
-    {
-    fullName: "Dr. Jane Smith",
-    email: "me@you.us",
-    image: "/img/team/travis.png",
-    googleScholarLink: "https://google.ca",
-    // lab: {
-    //   name: "HCI Lab",
-    //   website: "hci.cs.umanitoba.ca",
-    //   description: "HCI lol",
-    // },
-    researchInterests: ["Human Computer Interaction","Science Science Too Long to fit all onto one line like omg that's so sad"],
-    researchTopic: "Something something super good but is just too long to fit here but I want to see the dots show up lol",
-    acceptingStudents: true,
-    minStudentRequirements: ["GPA 5+"]
-  }
-]
