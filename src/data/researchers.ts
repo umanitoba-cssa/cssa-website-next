ac6451a927d8b9358e7f60c6a04633dce66858ca
arasantos <100260167+arasantos@users.noreply.github.com>
2025-10-18 17:03:14 -0500

    feat(TECH-37): Implement Reserchers Directory Landing Page
    Add reseracher landing page that first adds id to the researcher, then displays them in a grid
    Add some researcher information from the form to visualize how it looks on page
    Remove current research topic as no researcher has this information
    Make googleScholarLink optional as some prof didn't fill it out
    Add minor changes to researcher-card.tsx

--- src/data/researchers.ts
+++ src/data/researchers.ts
@@ -1,17 +1,57 @@
-export interface IResearcher { // researcher properties
-    fullName: string;
-    email: string;
-    image: string;
-    googleScholarLink: string; // link to google scholar
-    lab?: ILab; 
-    researchInterests: string[];
-    researchTopic: string; // current research topic
-    acceptingStudents: boolean;
-    minStudentRequirements: string[];
-}
-
 export interface ILab {
   name: string;
   website?: string;
   description: string;
 }
+
+const dataScienceLab: ILab = 
+{
+    name: "Data Science, Database & Data Mining Lab",
+    website: "dataScienceLab.com",
+    description: "Research within the lab focuses on data science, databases and data mining. Over the past decade, NSERC/Science USRA and UMVPRI/UMSU URA winners have conducted summer projects under academic supervision of Dr. Leung on the topics of efficient and effective management of, knowledge discovery from, as well as analysis of, various types of data."
+}
+
+const coreAILab: ILab = 
+{
+    name: "CORE AI Lab",
+    website: "dataScienceLab.com",
+    description: "Computer vision, Optimization, Responsibility, Artificial Intelligence"
+}
+
+export interface IResearcher { // researcher properties
+    id: number,
+    fullName: string;
+    email: string;
+    image: string;
+    googleScholarLink?: string; // link to google scholar
+    lab?: ILab; 
+    researchInterests: string[];
+    acceptingStudents: boolean;
+    minStudentRequirements: string[];
+}
+
+// If the researcher doesn't have requirements, put the following under minStudentRequirements
+const noRequirements: string = "This prof doesn't have any explicitly stated requirements. Try sending them an email."
+
+export const ResearchersInfo = [
+  {
+    fullName: "Dr. Carson Leung",
+    email: "Carson.Leung@UManitoba.ca",
+    image: "/img/researchers/carson-leung.jpg",
+    googleScholarLink: "https://scholar.google.ca/citations?hl=en&user=NJaW9s0AAAAJ",
+    lab: dataScienceLab,
+    researchInterests: ["Generative AI, ML, computer vision, optimization"],
+    acceptingStudents: true,
+    minStudentRequirements: ["Applicants must meet the minimum requirements for NSERC/Science USRA or UMVPRI/UMSU URA.", "Good interpersonal skills.", "Curious learner with a passion for data science."],
+  },
+  {
+    fullName: "Sadaf Salehkalaibar",
+    email: "sadaf.s86@gmail.com",
+    image: "/img/researchers/sadaf-salehkalaibar.jpg",
+    lab: coreAILab,
+    researchInterests: ["Generative AI, ML, computer vision, optimization"],
+    acceptingStudents: true,
+    minStudentRequirements: [noRequirements],
+  },
+];
+
