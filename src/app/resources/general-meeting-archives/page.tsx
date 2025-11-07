30a270f799e8b601f61a81acb11e7767287f5fec
cjlangan <connorjameslangan@gmail.com>
2025-11-06 21:58:45 -0600

    feat: added page template for general meetings

---
+++ src/app/resources/general-meeting-archives/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import PageHeader from "@/components/page-header";
+
+export default function GeneralMeetingArchives() {
+    return (
+        <PageHeader title="General Meeting Archives" image="/img/backgrounds/resources.png" />
+    );
+}
