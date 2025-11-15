fdea51a4baf871c12597b9a9f43e8898cfba1c60
Ikufisile Fopefoluwa <fope.ikufisile@gmail.com>
2025-11-15 15:09:02 -0600

    changed dummy years to start from 2024, installed the shadcn select component and implemented it to add year selection to the prev execs page

--- src/app/team/previous-execs/page.tsx
+++ src/app/team/previous-execs/page.tsx
@@ -1,17 +1,35 @@
 "use client";
-
 import BlockHeader from "@/components/block-header";
 import PageHeader from "@/components/page-header";
 import ProfileCard from "@/components/profile-card";
 import ProfileModal from "@/components/profile-modal";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { IProfile } from "@/data/team";
 import { ExecProfiles, filterByYear } from "@/data/prev-team";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { AnimatePresence } from "framer-motion";
 import Link from "next/link";
 
 export default function Team() {
-  const [choice, setChoice] = useState("2022");
+  const [choice, setChoice] = useState("2025");
+
+  const years = useMemo(() => {
+    const allYears = ExecProfiles.map((e) => e.year);
+    const min = Math.min(...allYears);
+    const max = Math.max(...allYears);
+
+    return Array.from({ length: max - min + 1 }, (_, i) =>
+      (min + i).toString()
+    );
+  }, []);
 
   const [selectedProfile, setSelectedProfile] = useState<IProfile | null>(null);
 
@@ -20,6 +38,20 @@ export default function Team() {
       <PageHeader title="Previous Execs" image="/img/backgrounds/team.png" />
       <div className="container py-12 flex flex-col gap-8">
         <BlockHeader title="Previous Executive Team" />
+        <Select value={choice} onValueChange={setChoice}>
+          <SelectTrigger className="w-[180px] focus:outline-none">
+            <SelectValue placeholder="Select a Year" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {years.map((year: string) => (
+                <SelectItem key={year} value={year}>
+                  {year}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
         <div className="flex flex-row gap-4 flex-wrap justify-center">
           {filterByYear(choice).map((profile) => (
             <ProfileCard
--- src/data/prev-team.ts
+++ src/data/prev-team.ts
@@ -3,13 +3,13 @@ export const ExecProfiles = [
     name: "Tofunmi Layi-Babatunde",
     position: "Director of Advocacy",
     image: "/img/team/prevExecs/tofunmi.png",
-    year: 2022,
+    year: 2024,
   },
   {
     name: "Edwin Duong",
     position: "Director of Internal Affairs",
     image: "/img/team/vacant.png",
-    year: 2023,
+    year: 2025,
   },
   {
     name: "Travis Friesen",
@@ -21,25 +21,25 @@ export const ExecProfiles = [
     name: "Chinu Chandaria",
     position: "Director of Events",
     image: "/img/team/prevExecs/chinu.png",
-    year: 2022,
+    year: 2025,
   },
   {
     name: "Ginelle Temull",
     position: "Director of Finance",
     image: "/img/team/prevExecs/ginelle.png",
-    year: 2025,
+    year: 2024,
   },
   {
     name: "KC del Moro",
     position: "Assistant Director of Promotions",
     image: "/img/team/execs/kc.JPG",
-    year: 2021,
+    year: 2025,
   },
   {
     name: "Okiki Fasanmi",
     position: "Vice President",
     image: "/img/team/prevExecs/okiki.png",
-    year: 2020,
+    year: 2024,
   },
 ];
 
