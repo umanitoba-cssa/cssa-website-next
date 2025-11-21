ddcd126c6ebd43bfe878c1b13b6b1383ed0d2d1b
Ikufisile Fopefoluwa <fope.ikufisile@gmail.com>
2025-11-20 19:33:52 -0600

    chore: Added guides and resources to home page, refactored home page dynamically

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import BlockHeader from "@/components/block-header";
 import PageHeader from "@/components/page-header";
 import Image from "next/image";
+import { Teasers } from "@/data/teasers";
+import Link from "next/link";
 
 export default function Home() {
     return (
@@ -22,65 +24,31 @@ export default function Home() {
                 <div className="flex flex-col gap-8">
                     <BlockHeader title="What we do" />
                     <div className="grid lg:grid-cols-[400px_auto] gap-12 lg:gap-8 items-center">
-                        <div className="aspect-3/2 align-middle relative">
+                        {Teasers.map((item)=>(
+                            <>
+                            <div className="aspect-3/2 align-middle relative">
                             <Image
                                 fill
-                                src="/img/teasers/advocacy.jpg"
-                                alt="Advocacy"
+                                src={item.src}
+                                alt={item.title}
                                 className="object-cover"
                             />
                         </div>
                         <div className="flex flex-col place-content-center gap-4">
-                            <h4>Advocacy</h4>
+                            <h4>{item.title}</h4>
                             <p>
-                                The CSSA is the official voice of computer science students at the University of
-                                Manitoba. We advocate for the interests and concerns of computer science
-                                students to the Department of Computer Science, the Faculty of Science, and the
-                                university administration. We work to ensure that computer science students have
-                                a high-quality education and a positive academic experience. We strive to
-                                improve the field by promoting diversity, inclusion, and equity in computing.
+                                {item.description}
                             </p>
+                            {item.href &&
+                            <Link href={item.href}>
+                                <div className="flex">
+                                    <span className="text-sm text-primary hover:underline"> {item.linkText} </span>
+                                </div>
+                            </Link>
+                            }
                         </div>
-
-                        <div className="aspect-3/2 align-middle relative">
-                            <Image
-                                fill
-                                src="/img/teasers/community.jpg"
-                                alt="Advocacy"
-                                className="object-cover"
-                            />
-                        </div>
-                        <div className="flex flex-col place-content-center gap-4">
-                            <h4>Community</h4>
-                            <p>
-                                We are a community of computer science students who love technology and
-                                learning. We help students connect with each other and the industry. We
-                                celebrate the diversity and achievements of our community. We also organize
-                                events and activities for computer science students to network, where they can
-                                build relationships that last beyond their university career. Come join us and
-                                visit the computer science lounge!
-                            </p>
-                        </div>
-
-                        <div className="aspect-3/2 align-middle relative">
-                            <Image
-                                fill
-                                src="/img/teasers/support.jpg"
-                                alt="Advocacy"
-                                className="object-cover"
-                            />
-                        </div>
-                        <div className="flex flex-col place-content-center gap-4">
-                            <h4>Support</h4>
-                            <p>
-                                Through representation, resources, and opportunities, the CSSA will empower the
-                                computer science student community to enhance their academic journey and their
-                                careers in the field of computing. We help computer science students with their
-                                academic challenges and questions, and also help students with their career
-                                plans and choices. We have resources and people who can assist students with
-                                their needs and interests.
-                            </p>
-                        </div>
+                        </>
+                        ))}
                     </div>
                 </div>
             </div>
