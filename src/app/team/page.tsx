12efcfa57331add8d532a2d8dd6e88361ea4d45f
Ikufisile Fopefoluwa <fope.ikufisile@gmail.com>
2025-12-11 22:50:33 -0600

    changed cardLinks style

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,22 +1,42 @@
 'use client';
 
-import BlockHeader from "@/components/block-header";
-import PageHeader from "@/components/page-header";
-import ProfileCard from "@/components/profile-card";
-import ProfileModal from "@/components/profile-modal";
-import { ExecProfiles, LoungeProfiles, PromotionsProfiles, FinanceProfiles, MerchProfiles, EventsProfiles, TechnologyProfiles, AdvocacyProfiles, StudentResourcesProfiles, IProfile } from "@/data/team";
-import { useState } from "react";
-import { AnimatePresence } from "framer-motion";
-import Link from "next/link";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-
+import BlockHeader from '@/components/block-header';
+import PageHeader from '@/components/page-header';
+import ProfileCard from '@/components/profile-card';
+import ProfileModal from '@/components/profile-modal';
+import {
+    ExecProfiles,
+    LoungeProfiles,
+    PromotionsProfiles,
+    FinanceProfiles,
+    MerchProfiles,
+    EventsProfiles,
+    TechnologyProfiles,
+    AdvocacyProfiles,
+    StudentResourcesProfiles,
+    IProfile,
+} from '@/data/team';
+import { useState } from 'react';
+import { AnimatePresence } from 'framer-motion';
+import Link from 'next/link';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
 
 export default function Team() {
     const [selectedProfile, setSelectedProfile] = useState<IProfile | null>(null);
 
     return (
         <main className="flex flex-col">
-            <PageHeader title="Team" image="/img/backgrounds/team.png" />
+            <PageHeader
+                title="Team"
+                image="/img/backgrounds/team.png"
+            />
             <div className="container py-12 flex flex-col gap-8">
                 <BlockHeader title="Executive Team" />
                 <div className="flex flex-row gap-4 flex-wrap justify-center">
@@ -30,7 +50,6 @@ export default function Team() {
                         />
                     ))}
                 </div>
-   
 
                 <BlockHeader title="Promotions Committee" />
                 <div className="flex flex-row gap-4 flex-wrap justify-center">
@@ -125,25 +144,25 @@ export default function Team() {
 
                 <BlockHeader title="Previous Team" />
                 <div className="flex items-center justify-center">
-                <div className="px-2 w-full md:w-2/5 h-full">
-                    <Link href={"/team/previous-team"}>
-                <Card className="h-full border-primary">
-                            <CardHeader>
-                                <CardTitle className="text-lg flex flex-row gap-2">
-                                    Previous Team
-                                </CardTitle>
-                                <CardDescription>View the Previous Team Page</CardDescription>
-                            </CardHeader>
-                            <CardContent>
-                              <div className="flex justify-end">
-                                <span className="text-sm text-primary hover:underline">
-                                  View Previous Team →
-                                </span>
-                              </div>
-                            </CardContent>
-                        </Card>
+                    <div className="px-2 w-[270px] h-full">
+                        <Link href={'/team/previous-team'}>
+                            <Card className="h-full border-primary">
+                                <CardHeader>
+                                    <CardTitle className="text-lg flex flex-row gap-2">
+                                        Previous Team
+                                    </CardTitle>
+                                    <CardDescription>View the Previous Team Page</CardDescription>
+                                </CardHeader>
+                                <CardContent>
+                                    <div className="flex justify-end">
+                                        <span className="text-sm text-primary hover:underline">
+                                            View Previous Team →
+                                        </span>
+                                    </div>
+                                </CardContent>
+                            </Card>
                         </Link>
-                        </div>
+                    </div>
                 </div>
             </div>
 
--- src/app/team/previous-team/page.tsx
+++ src/app/team/previous-team/page.tsx
@@ -1,103 +1,114 @@
-"use client";
-import BlockHeader from "@/components/block-header";
-import PageHeader from "@/components/page-header";
-import ProfileCard from "@/components/profile-card";
-import ProfileModal from "@/components/profile-modal";
+'use client';
+import BlockHeader from '@/components/block-header';
+import PageHeader from '@/components/page-header';
+import ProfileCard from '@/components/profile-card';
+import ProfileModal from '@/components/profile-modal';
 import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { IProfile } from "@/data/team";
-import { ExecProfiles, filterByYear } from "@/data/prev-team";
-import { useState, useMemo } from "react";
-import { AnimatePresence } from "framer-motion";
-import Link from "next/link";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select';
+import { IProfile } from '@/data/team';
+import { ExecProfiles, filterByYear } from '@/data/prev-team';
+import { useState, useMemo } from 'react';
+import { AnimatePresence } from 'framer-motion';
+import Link from 'next/link';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
 
 export default function PreviousTeam() {
-  const [choice, setChoice] = useState("2025");
+    const [choice, setChoice] = useState('2025');
 
-  const years = useMemo(() => {
-    const allYears = ExecProfiles.map((e) => e.year);
-    const min = Math.min(...allYears);
-    const max = Math.max(...allYears);
+    const years = useMemo(() => {
+        const allYears = ExecProfiles.map((e) => e.year);
+        const min = Math.min(...allYears);
+        const max = Math.max(...allYears);
 
-    return Array.from({ length: max - min + 1 }, (_, i) =>
-      (min + i).toString()
-    );
-  }, []);
+        return Array.from({ length: max - min + 1 }, (_, i) => (min + i).toString());
+    }, []);
 
-  const [selectedProfile, setSelectedProfile] = useState<IProfile | null>(null);
+    const [selectedProfile, setSelectedProfile] = useState<IProfile | null>(null);
 
-  return (
-    <main className="flex flex-col">
-      <PageHeader title="Previous Team" image="/img/backgrounds/team.png" />
-      <div className="container py-12 flex flex-col gap-8">
-        <BlockHeader title="Previous Team" />
-        <Select value={choice} onValueChange={setChoice}>
-          <SelectTrigger className="w-[180px] focus:outline-none">
-            <SelectValue placeholder="Select a Year" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              {years.map((year: string) => (
-                <SelectItem key={year} value={year}>
-                  {year}
-                </SelectItem>
-              ))}
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-        <div className="flex flex-row gap-4 flex-wrap justify-center">
-          {filterByYear(choice).map((profile) => (
-            <ProfileCard
-              key={`${profile.name}-${profile.position}`}
-              profile={profile}
-              onClick={() => {
-                setSelectedProfile(profile);
-              }}
+    return (
+        <main className="flex flex-col">
+            <PageHeader
+                title="Previous Team"
+                image="/img/backgrounds/team.png"
             />
-          ))}
-        </div>
+            <div className="container py-12 flex flex-col gap-8">
+                <BlockHeader title="Previous Team" />
+                <Select
+                    value={choice}
+                    onValueChange={setChoice}>
+                    <SelectTrigger className="w-[180px] focus:outline-none">
+                        <SelectValue placeholder="Select a Year" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            {years.map((year: string) => (
+                                <SelectItem
+                                    key={year}
+                                    value={year}>
+                                    {year}
+                                </SelectItem>
+                            ))}
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+                <div className="flex flex-row gap-4 flex-wrap justify-center">
+                    {filterByYear(choice).map((profile) => (
+                        <ProfileCard
+                            key={`${profile.name}-${profile.position}`}
+                            profile={profile}
+                            onClick={() => {
+                                setSelectedProfile(profile);
+                            }}
+                        />
+                    ))}
+                </div>
 
-                        <BlockHeader title="Current Team" />
-                        <div className="flex items-center justify-center">
-                        <div className="px-2 w-full md:w-2/5 h-full">
-                            <Link href={"/team"}>
-                        <Card className="h-full border-primary">
-                                    <CardHeader>
-                                        <CardTitle className="text-lg flex flex-row gap-2">
-                                            Current Team
-                                        </CardTitle>
-                                        <CardDescription>View the Current Team Page</CardDescription>
-                                    </CardHeader>
-                                    <CardContent>
-                                      <div className="flex justify-end">
+                <BlockHeader title="Current Team" />
+                <div className="flex items-center justify-center">
+                    <div className="px-2 w-[270px]  h-full">
+                        <Link href={'/team'}>
+                            <Card className="h-full border-primary">
+                                <CardHeader>
+                                    <CardTitle className="text-lg flex flex-row gap-2">
+                                        Current Team
+                                    </CardTitle>
+                                    <CardDescription>View the Current Team Page</CardDescription>
+                                </CardHeader>
+                                <CardContent>
+                                    <div className="flex justify-end">
                                         <span className="text-sm text-primary hover:underline">
-                                          View Current Team →
+                                            View Current Team →
                                         </span>
-                                      </div>
-                                    </CardContent>
-                                </Card>
-                                </Link>
-                                </div>
-                        </div>
-      </div>
+                                    </div>
+                                </CardContent>
+                            </Card>
+                        </Link>
+                    </div>
+                </div>
+            </div>
 
-      <AnimatePresence>
-        {selectedProfile && (
-          <ProfileModal
-            profile={selectedProfile}
-            onClose={() => setSelectedProfile(null)}
-          />
-        )}
-      </AnimatePresence>
-    </main>
-  );
+            <AnimatePresence>
+                {selectedProfile && (
+                    <ProfileModal
+                        profile={selectedProfile}
+                        onClose={() => setSelectedProfile(null)}
+                    />
+                )}
+            </AnimatePresence>
+        </main>
+    );
 }
