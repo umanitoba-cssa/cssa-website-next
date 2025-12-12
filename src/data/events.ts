533eb1a658f37fa52fdec220193b40c92b8de809
cjlangan <connorjameslangan@gmail.com>
2025-12-12 17:52:34 -0600

    feat: added event page and carousels and event data architected photo storage

---
+++ src/data/events.ts
@@ -0,0 +1,61 @@
+interface IEventLink {
+    title: string;
+    description: string;
+    href: string;
+    internal?: boolean;
+    linkText?: string;
+}
+
+export const RecentEvents: IEventLink[] = [
+    {
+        title: 'Game Jam 2025',
+        description:
+            'Compete to build a game with the theme "Evolution"',
+        href: 'gamejam',
+        internal: true,
+        linkText: 'See Page →'
+    },
+    {
+        title: 'Capture The Flag 2025',
+        description:
+            'Compete with a team in cybersecurity challenges',
+        href: 'events/capturetheflag2025',
+        internal: true,
+        linkText: 'View Photos →'
+    },
+    {
+        title: 'Goosies  2024',
+        description:
+            'Annual award show for the Department of Computer Science',
+        href: 'events/goosies2024',
+        internal: true,
+        linkText: 'View Photos →'
+    },
+    {
+        title: 'Bonfire 2024',
+        description:
+            'Hang around the campfire with the CSSA',
+        href: 'events/bonfire2024',
+        internal: true,
+        linkText: 'View Photos →'
+    },
+]
+
+export const OlderEvents: IEventLink[] = [
+    {
+        title: 'Game Jam 2024',
+        description:
+            'Compete with a team to build a game',
+        href: 'events/gamejam2024',
+        internal: true,
+        linkText: 'See Page →'
+    },
+    {
+        title: 'Goosies  2023',
+        description:
+            'Annual award show for CS Professors',
+        href: 'events/goosies2023',
+        internal: true,
+        linkText: 'View Photos →'
+    },
+]
