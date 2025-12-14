ef3d6cfac7980e321fb7ef700c819f1eed96739d
iya <117234851+iyfrncsca@users.noreply.github.com>
2025-12-13 19:06:02 -0600

    TECH 32: Change color of text in student guides (#77)

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -8,14 +8,14 @@ export default function PageHeader({ title, image }: { title: string; image: str
                     <Image className="relative object-cover" fill src={image} alt={title} />
                     <div className="w-full h-full bg-opacity-30 bg-black p-12 relative">
                         <div className="container z-10">
-                            <h2 className="border-l-4 p-8 border-cssa-gold">{title}</h2>
+                            <h2 className="border-l-4 p-8 border-cssa-gold text-white">{title}</h2>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="mobile-only flex flex-col gap-4">
                 <div className="container">
-                    <h2 className="text-3xl">{title}</h2>
+                    <h2 className="text-3xl text-white">{title}</h2>
                 </div>
                 <div className="w-full h-48 relative">
                     <Image className="relative object-cover" fill src={image} alt={title} />
