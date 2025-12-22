fb0d14f3be52197a0a8e587132ad5b9100a880c7
Aidan McLeod <45048893+ACM02@users.noreply.github.com>
2025-12-22 10:52:27 -0600

    More auto fixes

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -5,7 +5,12 @@ export default function PageHeader({ title, image }: { title: string; image: str
         <>
             <div className="desktop-only">
                 <div className="w-full h-[500px] overflow-hidden relative">
-                    <Image className="relative object-cover" fill src={image} alt={title} />
+                    <Image
+                        className="relative object-cover"
+                        fill
+                        src={image}
+                        alt={title}
+                    />
                     <div className="w-full h-full bg-opacity-30 bg-black p-12 relative">
                         <div className="container z-10">
                             <h2 className="border-l-4 p-8 border-cssa-gold">{title}</h2>
@@ -18,7 +23,12 @@ export default function PageHeader({ title, image }: { title: string; image: str
                     <h2 className="text-3xl">{title}</h2>
                 </div>
                 <div className="w-full h-48 relative">
-                    <Image className="relative object-cover" fill src={image} alt={title} />
+                    <Image
+                        className="relative object-cover"
+                        fill
+                        src={image}
+                        alt={title}
+                    />
                 </div>
             </div>
         </>
