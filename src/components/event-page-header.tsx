da98b1da12f2b5637468a4c8ac63b4537157655b
Aidan McLeod <45048893+ACM02@users.noreply.github.com>
2025-12-22 10:54:14 -0600

    A few more fixes

--- src/components/event-page-header.tsx
+++ src/components/event-page-header.tsx
@@ -5,13 +5,23 @@ export default function EventPageHeader({ title, image }: { title: string; image
         <>
             <div className="desktop-only">
                 <div className="w-full max-w-[1920px] mx-auto h-[800px] overflow-hidden relative">
-                    <Image className="relative object-cover" fill src={image} alt={title} />
+                    <Image
+                        className="relative object-cover"
+                        fill
+                        src={image}
+                        alt={title}
+                    />
                     <div className="w-full h-full p-12 relative"></div>
                 </div>
             </div>
             <div className="mobile-only flex flex-col gap-4">
                 <div className="w-full h-48 relative">
-                    <Image className="relative object-cover" fill src={image} alt={title} />
+                    <Image
+                        className="relative object-cover"
+                        fill
+                        src={image}
+                        alt={title}
+                    />
                 </div>
             </div>
         </>
