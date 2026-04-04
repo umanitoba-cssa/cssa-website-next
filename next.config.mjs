3a37ef922264648101a8d3862239541c509f618c
Ara Nicole Santos <aranicole_santos@outlook.com>
2026-04-04 05:46:08 -0500

    Add images config to manage home Image component and optimization are handled

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        imageSizes: [16, 32, 48, 64, 96, 128, 160, 208, 256, 320, 384, 416, 512],
+        deviceSizes: [320, 375, 393, 400, 430, 480, 512, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+        qualities: [50, 75],
+    },
+};
 
 export default nextConfig;
