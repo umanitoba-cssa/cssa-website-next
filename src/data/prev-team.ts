878fa811c52c5746c8183b3e3960df1af0975c17
Aidan McLeod <45048893+ACM02@users.noreply.github.com>
2026-08-10 20:41:03 -0500

    Add Peter to past execs section

--- src/data/prev-team.ts
+++ src/data/prev-team.ts
@@ -57,6 +57,14 @@ export const PrevExecsProfiles = [
         image: '/img/team/execs/kc.JPG',
         year: '2024 - 2025',
     },
+    {
+        name: 'Peter Vu',
+        position: 'Director of Student Affairs',
+        image: '/img/team/prevExecs/peter.jpg',
+        linkedin: 'https://linkedin.com/in/petervudev',
+        github: 'https://github.com/pieberrykinnie',
+        year: '2024 - 2025',
+    },
 ];
 
 export const filterByYear = (profile: IProfile[], year: string) => {
